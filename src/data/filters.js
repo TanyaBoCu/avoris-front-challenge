c11975321006c268c678e60e9eb4caa291af2263
Tanya Botella <ocean77delfin@hotmail.com>
2026-03-25 00:49:11 +0100

    feat: add mocked filters data

---
+++ src/data/filters.js
@@ -0,0 +1,23 @@
+export const filters = {
+  destinations: [
+    { label: 'África', value: 'africa', info: 'Lorem ipsum' },
+    { label: 'Asia', value: 'asia', info: 'Lorem ipsum' },
+    { label: 'Europa', value: 'europa', info: 'Lorem ipsum' },
+    { label: 'N. América', value: 'namerica', info: 'Lorem ipsum' }
+  ],
+  categories: [
+    { label: 'Quads', value: 'quads', info: 'Lorem ipsum' },
+    { label: 'Parapente', value: 'parapente', info: 'Lorem ipsum' },
+    { label: 'Rafting', value: 'rafting', info: 'Lorem ipsum' },
+    { label: 'Explora', value: 'explora', info: 'Lorem ipsum' },
+    { label: 'Buceo', value: 'buceo', info: 'Lorem ipsum' },
+    { label: 'Paracaídas', value: 'paracaidas', info: 'Lorem ipsum' },
+    { label: 'Snowboard', value: 'snowboard', info: 'Lorem ipsum' },
+    { label: 'Surf', value: 'surf', info: 'Lorem ipsum' }
+  ],
+  stays: [
+    { label: 'Hotel', value: 'hotel', info: 'Lorem ipsum' },
+    { label: 'Hostel', value: 'hostal', info: 'Lorem ipsum' },
+    { label: 'Apartamento', value: 'apartamento', info: 'Lorem ipsum' }
+  ]
+};
